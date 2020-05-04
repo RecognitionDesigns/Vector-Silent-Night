a7998fdf0ddabc7fd461f39c89b44a743f301909
Colin Twigg <colin.f.twigg@gmail.com>
2020-05-04 21:29:23 +0100

    Add files via upload

---
+++ silent_night.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import anki_vector
+from anki_vector.util import degrees
+import time
+import sys
+
+hours_to_sleep = 8
+
+with anki_vector.Robot('0050169f') as robot:
+    robot.conn.release_control()
+    while True:
+        if robot.touch.last_sensor_reading.is_being_touched:
+            robot.conn.request_control()
+            time.sleep(2)
+            robot.behavior.say_text("Good night, I am going home")
+            robot.behavior.drive_on_charger()
+            robot.behavior.set_head_angle(degrees(-22.0))
+
+            duration_s = (hours_to_sleep * 3600)
+            robot.screen.set_screen_to_color(anki_vector.color.Color(rgb=[0, 0, 0]), duration_sec=duration_s)
+            robot.behavior.say_text("Commencing {} hour deep sleep".format(hours_to_sleep))
+            time.sleep(duration_s)
+            sys.exit()
+
